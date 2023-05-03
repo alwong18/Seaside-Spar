e00ef0794ecae7f24d0fa41d115ebb00d88806f4
amwu5 <121971706+amwu5@users.noreply.github.com>
2023-05-03 11:02:19 -0700

    particle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     public bool gameOver;
     
     public GameObject projectilePrefab;
+    public ParticleSystem collisionParticle;
 
     // Start is called before the first frame update
     void Start()
