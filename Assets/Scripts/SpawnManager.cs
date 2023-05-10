e7070a2334a1c89217e146ceaf1ab48a0ac6ebc7
danicachow <121971462+danicachow@users.noreply.github.com>
2023-05-10 11:04:10 -0700

    update spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,12 +10,18 @@ public class SpawnManager : MonoBehaviour
     private float spawnPosZ = 20;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
+    private int peopleIndex;
+    private Vector3 spawnPos;
+    private int obstacleIndex;
+
 
     private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        SpawnEnemyWave(2);
+
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnRandomPeople", startDelay, spawnInterval);
         InvokeRepeating("SpawnRandomObstacles", startDelay, spawnInterval);
@@ -27,14 +33,28 @@ public class SpawnManager : MonoBehaviour
       
     }
 
-    void SpawnRandomPeople()
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            int peopleIndex = Random.Range(0, peoplePrefabs.Length);
+            Instantiate(peoplePrefabs[peopleIndex], spawnPos, peoplePrefabs[peopleIndex].transform.rotation);
+
+            int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+        }
+    }
+
+    /*void SpawnRandomPeople()
     {
         if(playerControllerScript.gameOver == false)
         {
-            int peopleIndex = Random.Range(0, peoplePrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            //int peopleIndex = Random.Range(0, peoplePrefabs.Length);
+            //Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
         
-            Instantiate(peoplePrefabs[peopleIndex], spawnPos, peoplePrefabs[peopleIndex].transform.rotation);
+            //Instantiate(peoplePrefabs[peopleIndex], spawnPos, peoplePrefabs[peopleIndex].transform.rotation);
         }
     }
 
@@ -43,11 +63,11 @@ public class SpawnManager : MonoBehaviour
     {
         if(playerControllerScript.gameOver == false)
         {
-            int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            //int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+            //Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
         
-            Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+            //Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
         }   
-    }
+    }*/
 }
 
